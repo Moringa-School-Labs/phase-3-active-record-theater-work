d78c1ffdd4c36e42195643197f5445bd4be4458e
catherine <vuthi.esther@gmail.com>
2022-06-20 22:54:19 +0300

    completed assignment

--- app/models/audition.rb
+++ app/models/audition.rb
@@ -1,3 +1,14 @@
 class Audition < ActiveRecord::Base
-  
+  belongs_to :role
+
+  def role
+    Role.all.each do |each_role|
+        puts each_role
+    end
+  end
+
+  def call_back
+    self.update(hired: true)
+  end
+
 end
---
+++ app/models/role.rb
@@ -0,0 +1,57 @@
+class Role < ActiveRecord::Base
+  has_many :auditions
+
+    def auditions
+        self.auditions
+    end
+
+    def actors 
+        return self.auditions.map do | each_audition |
+            each_audition.actor
+        end
+    end
+
+    def locations 
+        actors_location = []
+        self.auditions.each do |audition|
+            actors_location << audition.location
+        end
+        return actors_location
+    end
+
+    def lead 
+         array_result_of_filter = self.auditions.filter do | that_audtion_instance |
+            
+            that_audtion_instance.hired == true
+
+        end
+
+
+        if (array_result_of_filter.size != 0  &&  array_result_of_filter.size > 0)
+
+            return array_result_of_filter.first
+        
+        else 
+            return 'no actor has been hired for this role'
+        end
+    end
+
+    def understudy 
+        array_result_of_filter = self.auditions.filter do | that_audtion_instance |
+            
+            that_audtion_instance.hired == true
+
+        end
+
+
+        if (array_result_of_filter.size != 0  &&  array_result_of_filter.size > 1)
+
+            return array_result_of_filter.second
+        
+        else 
+            return 'no actor has been hired for understudy for this role'
+        end
+    end
+
+
+end
